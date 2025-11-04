cb6a7052453c0d256b890d30f3f5717b36aae230
stringerc <stringer.c.a@gmail.com>
2025-11-03 22:05:25 -0500

    Deploy Next.js - add next.config.js

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,4 +1,4 @@
-/** @type {import("next").NextConfig} */
+/** @type { import("next").NextConfig } */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -9,4 +9,5 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
